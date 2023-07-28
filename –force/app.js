b014da7ea160b9c1feb841ec3492c8c850eec104
hanna <hanna-881@hotmail.com>
2023-07-27 21:34:07 -0400

    module2

--- –force/app.js
+++ –force/app.js
@@ -6,12 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const travelRouter = require('./app_server/routes/travel');
 var app = express();
 
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// register handlebars partials
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+
 app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
