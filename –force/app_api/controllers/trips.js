ecf840f72be1ecbdce03835a21254807e26752ca
hanna <hanna-881@hotmail.com>
2023-07-30 18:30:06 -0400

    module5

--- –force/app_api/controllers/trips.js
+++ –force/app_api/controllers/trips.js
@@ -1,36 +1,53 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require.path('path');
-const cookieParsers = require('cookie-parser');
-const logger = require('morgan');
-const hbs = require('hbs');
-require('./app_api/models/db');
+const mongoose = require('mongoose');
+const model = mongoose.model('trips');
 
-const indexRouter = require('./app_api/models/index');
-const usersRouter = require('./app_api/models/users');
-const travelRouter = require('./app_api/models/travel');
-const apiRouter = require('./app_api/models/index');
+// GET: /trips
+
+const getAllTrips = async (req, res) => {
+    model
+        .find({})
+        .exec((err, trips) => {
+            if(!trips) {
+                return res
+                    .status(404)
+                    .json({ "message": "trips not found"});
+
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            } else {
+                return res 
+                    .status(200)
+                    .json(trips);
+            }
+        });
+};
 
 
-const app = express();
+const tripsFindCode = async (req, res) => {
+    model
+        .find({ 'code': req.params.tripCode })
+        .exec((err, trip) => {
+            if (!trip) {
+                return res  
+                    .status(404)
+                    .json({"message": "That trip was not found"});
 
-app.set('views', path.join(_dirname, 'app_server', 'vievs'));
+            } else if (err) {
+                return res  
+                    .status(404)
+                    .json(err);
+            } else {
+                return res
+                    .status(200)
+                    .json(trip)
 
-hbs.registerPartials(path.join(_dirname, 'app_server', 'views/partials'));
+            }
+        });
+};
 
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: falce}));
-app.use(cookieParsers());
-app.use(express.static(path.join(_dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/travel', travelRouter);
-app.use('/api', apiRouter);
-
-app.use(function(req, res, next) {
-    next(createError(404));
-})
+module.exports = {
+    getAllTrips,
+    tripsFindCode
+};
